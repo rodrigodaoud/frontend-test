fd36c9ae8a071975012aafa39cd928aa29d737c1
Rodrigo <rodaoud93@gmail.com>
2018-04-08 19:33:08 +0200

    fixex layout.js to create product cards

--- src/js/layout.js
+++ src/js/layout.js
@@ -4,64 +4,70 @@ function main(data){
 
     const productListMainElement = document.getElementById('products-list');
 
+    function createCardElement (data, i){
+        const productCardElement = document.createElement('div');
+        productCardElement.classList.add('product-card');
+        productListMainElement.appendChild(productCardElement);
+        createCardLeftElements(data, i, productCardElement);
+        createCardRightElements(data, i, productCardElement);
+    }
 
-    function createProductCard(data){
-        for(let i = 0; i < products.length; i++){
+    function createCardLeftElements (data, i, productCardElement){
+        // const productCardElement = document.getElementsByClassName('product-card');
+        const cardLeftElement = document.createElement('div');
+        const productImageElement = document.createElement('img');
+        const productInfoElement = document.createElement('div');
+        const InfoDivElement = document.createElement('div');
+        const productNameElement = document.createElement('h2');
+        const productColorElement = document.createElement('p');
+        const productInputElement = document.createElement('div');
+        const productQuantityElement = document.createElement('input');
+        productImageElement.src = products[i].image;
+        productNameElement.innerText = products[i].name;
+        productColorElement.innerText = products[i].color;
+        productQuantityElement.type = 'number';
+        productQuantityElement.value = '0';
+        cardLeftElement.classList.add('card-left');
+        productInfoElement.classList.add('card-info');
+        cardLeftElement.appendChild(productImageElement);
+        cardLeftElement.appendChild(productInfoElement);
+        InfoDivElement.appendChild(productNameElement);
+        InfoDivElement.appendChild(productColorElement);
+        productInfoElement.appendChild(InfoDivElement);
+        productInfoElement.appendChild(productInputElement);
+        productInputElement.appendChild(productQuantityElement);
+        productCardElement.appendChild(cardLeftElement);
+        // productListMainElement.appendChild(cardLeftElement);
+    }
 
-            const productCardElement = document.createElement('div');
-            const cardLeftElement = document.createElement('div');
-            const cardRightElement = document.createElement('div');
-            // --- Card Left Element --- //
-            const productImageElement = document.createElement('img');
-            const productInfoElement = document.createElement('div');
-            const InfoDivElement = document.createElement('div');
-            const productNameElement = document.createElement('h2');
-            const productColorElement = document.createElement('p');
-            const productInputElement = document.createElement('div');
-            const productQuantityElement = document.createElement('input');
-            // --- Card Right Element --- //
-            const productPriceElement = document.createElement('p');
-            const productPriceValueElement = document.createElement('span');
-            const productButtonElement = document.createElement('button');
+    function createCardRightElements (data, i, productCardElement){
+        const cardRightElement = document.createElement('div');
+        const productPriceElement = document.createElement('p');
+        const productPriceValueElement = document.createElement('span');
+        const productButtonElement = document.createElement('button');
+        cardRightElement.className = 'card-right';
+        productPriceElement.innerText = '$';
+        productPriceValueElement.innerText = products[i].price;
+        productButtonElement.innerText = 'Add to cart';
+        cardRightElement.appendChild(productPriceElement);
+        cardRightElement.appendChild(productButtonElement);
+        productPriceElement.appendChild(productPriceValueElement);
+        productCardElement.appendChild(cardRightElement);
+        // productListMainElement.appendChild(cardRightElement);
+    }
 
-            cardLeftElement.className = 'card-left';
-            cardRightElement.className = 'card-right';
-            productCardElement.className = 'product-card';
-            productInfoElement.className = 'card-info';
-
-            //  --- Card Left Element --- //
-            productImageElement.src = products[i].image;
-            productNameElement.innerText = products[i].name;
-            productColorElement.innerText = products[i].color;
-            productQuantityElement.type = 'number';
-            productQuantityElement.value = '0';
-            //  --- Card Right Element --- //
-            productPriceElement.innerText = '$';
-            productPriceValueElement.innerText = products[i].price;
-            productButtonElement.innerText = 'Add to cart';
-
-            productListMainElement.appendChild(productCardElement);
-            productCardElement.appendChild(cardLeftElement);
-            productCardElement.appendChild(cardRightElement);
-            // --- Card Left Element --- //
-            cardLeftElement.appendChild(productImageElement);
-            cardLeftElement.appendChild(productInfoElement);
-            InfoDivElement.appendChild(productNameElement);
-            InfoDivElement.appendChild(productColorElement);
-            productInfoElement.appendChild(InfoDivElement);
-            productInfoElement.appendChild(productInputElement);
-            productInputElement.appendChild(productQuantityElement);
-            // -- Card Right Element --- /
-            cardRightElement.appendChild(productPriceElement);
-            cardRightElement.appendChild(productButtonElement);
-            productPriceElement.appendChild(productPriceValueElement);
-
-
-            
+    function displayProducts(data) {
+        for (let i = 0; i < products.length; i++) {
+            createCardElement(data, i);
         }
     }
 
-    createProductCard(data);
+    function createShoppingCart(){
+        
+    }
+
+    displayProducts(data);
+    createShoppingCart();
 }
 
 document.addEventListener('DOMContentLoad', main(products));
