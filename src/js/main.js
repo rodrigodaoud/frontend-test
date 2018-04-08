789246e91971f9d90f27f634825a6f664f74e5dc
Rodrigo <rodaoud93@gmail.com>
2018-04-09 01:28:06 +0200

    trying to figure out the remove button

--- src/js/main.js
+++ src/js/main.js
@@ -4,36 +4,42 @@ function main(){
 
   const errorMessageElement = document.getElementById('empty-cart');
   const addToCartButtonElements = document.querySelectorAll('#add-btn');
-  const shoppingCartElement = document.querySelector('.final-card'); 
+  const shoppingCartElement = document.querySelector('.final-card');
+  const removeButtonElement = document.createElement('button');
+  const removeButtons = document.querySelectorAll('#remove-btn');
+  console.log(removeButtonElement);
 
-  function createFinalPriceElements(calculatedFinalPriceElement) {
+  function createFinalPriceElements(finalPrice) {
     const finalPriceContainerElement = document.createElement('div');
-    const removeButtonElement = document.createElement('button');
     const totalPriceElement = document.createElement('p');
 
     finalPriceContainerElement.classList.add('final-price');
     removeButtonElement.innerText = 'X';
     removeButtonElement.setAttribute('id', 'remove-btn');
-    totalPriceElement.innerText = calculatedFinalPriceElement;
+    totalPriceElement.innerText = finalPrice;
 
     finalPriceContainerElement.appendChild(removeButtonElement);
     finalPriceContainerElement.appendChild(totalPriceElement);
     shoppingCartElement.appendChild(finalPriceContainerElement);
   }
+  
+  function handleRemoveItem(e){
+    console.log('hello');
+  }
 
   function handleAddToCart(e){
     const quantityInputValue = (e.currentTarget.parentNode.parentNode.firstChild.lastChild.lastChild.firstChild.value);
-    
+
     if(quantityInputValue != 0){
       errorMessageElement.style = 'display: none';
+
       const calculatedFinalPriceElement = (e.currentTarget.parentNode.firstChild.lastChild.innerText) * quantityInputValue;
       const productInfo = (e.currentTarget.parentNode.parentNode.childNodes[0]);
       const cloneProductInfo = productInfo.cloneNode(true);
-
+      
       shoppingCartElement.appendChild(cloneProductInfo);
-
+      
       createFinalPriceElements(calculatedFinalPriceElement);
-      console.log(e.currentTarget.parentNode.parentNode.firstChild.lastChild.lastChild.firstChild.value);
     }
     if(quantityInputValue == 0){
       alert('The product quantity must be more than 0');
@@ -43,6 +49,8 @@ function main(){
   for (let i = 0; i < addToCartButtonElements.length; i++){
     addToCartButtonElements[i].addEventListener('click', handleAddToCart);
   }
+  for (let i = 0; i < removeButtonElement.length; i++) {
+    removeButtonElement[i].addEventListener('click', handleRemoveItem);
+  }
 }
-
-document.addEventListener('DOMContentLoaded', main);
+document.addEventListener('DOMContentLoaded', main)
