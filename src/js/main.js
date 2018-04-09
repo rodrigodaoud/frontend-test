ba7424434c9a206d1334f5ac529d6b9fd22e98b1
Rodrigo <rodaoud93@gmail.com>
2018-04-09 02:40:34 +0200

    remove button working

--- src/js/main.js
+++ src/js/main.js
@@ -4,53 +4,61 @@ function main(){
 
   const errorMessageElement = document.getElementById('empty-cart');
   const addToCartButtonElements = document.querySelectorAll('#add-btn');
-  const shoppingCartElement = document.querySelector('.final-card');
-  const removeButtonElement = document.createElement('button');
-  const removeButtons = document.querySelectorAll('#remove-btn');
-  console.log(removeButtonElement);
-
+  const shoppingCartElement = document.querySelector('.final-products');
+  let finalProductCardElement;
+  let removeButtonElement;
+  let removeButtons;
+  let productInfo
+  let cloneProductInfo;
+  
   function createFinalPriceElements(finalPrice) {
+    finalProductCardElement = document.createElement('div');
+    removeButtonElement = document.createElement('button');
     const finalPriceContainerElement = document.createElement('div');
     const totalPriceElement = document.createElement('p');
-
+    
     finalPriceContainerElement.classList.add('final-price');
+    finalProductCardElement.setAttribute('class', 'final-card');
     removeButtonElement.innerText = 'X';
     removeButtonElement.setAttribute('id', 'remove-btn');
     totalPriceElement.innerText = finalPrice;
-
+    
+    shoppingCartElement.appendChild(finalProductCardElement);
+    finalProductCardElement.appendChild(cloneProductInfo);
     finalPriceContainerElement.appendChild(removeButtonElement);
     finalPriceContainerElement.appendChild(totalPriceElement);
-    shoppingCartElement.appendChild(finalPriceContainerElement);
+    finalProductCardElement.appendChild(finalPriceContainerElement);
+    removeButtons = document.querySelectorAll('#remove-btn');
+    for (let i = 0; i < removeButtons.length; i++) {
+      removeButtons[i].addEventListener('click', handleRemoveItem);
+    }
   }
   
   function handleRemoveItem(e){
-    console.log('hello');
+    e.currentTarget.parentNode.parentNode.remove();
   }
 
   function handleAddToCart(e){
     const quantityInputValue = (e.currentTarget.parentNode.parentNode.firstChild.lastChild.lastChild.firstChild.value);
-
     if(quantityInputValue != 0){
       errorMessageElement.style = 'display: none';
-
-      const calculatedFinalPriceElement = (e.currentTarget.parentNode.firstChild.lastChild.innerText) * quantityInputValue;
-      const productInfo = (e.currentTarget.parentNode.parentNode.childNodes[0]);
-      const cloneProductInfo = productInfo.cloneNode(true);
       
-      shoppingCartElement.appendChild(cloneProductInfo);
+      const calculatedFinalPriceElement = (e.currentTarget.parentNode.firstChild.lastChild.innerText) * quantityInputValue;
+      productInfo = (e.currentTarget.parentNode.parentNode.childNodes[0]);
+      cloneProductInfo = productInfo.cloneNode(true);
       
       createFinalPriceElements(calculatedFinalPriceElement);
     }
-    if(quantityInputValue == 0){
+    else if(quantityInputValue == 0){
       alert('The product quantity must be more than 0');
     }
-  }
+  } 
 
-  for (let i = 0; i < addToCartButtonElements.length; i++){
-    addToCartButtonElements[i].addEventListener('click', handleAddToCart);
+  function addEventHandler(){
+    for (let i = 0; i < addToCartButtonElements.length; i++){
+      addToCartButtonElements[i].addEventListener('click', handleAddToCart);
+    }
   }
-  for (let i = 0; i < removeButtonElement.length; i++) {
-    removeButtonElement[i].addEventListener('click', handleRemoveItem);
-  }
+  addEventHandler();
 }
 document.addEventListener('DOMContentLoaded', main)
